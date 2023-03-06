64691b852d4fca9a192dc4332c2bc0ff14a75250
Raul Kevin Aliaga Shapiama <45242019+Ra0l@users.noreply.github.com>
2023-03-06 09:57:59 -0500

    create project

---
+++ PostApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostApp
+//
+//  Created by Raul Kevin Aliaga Shapiama on 3/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
